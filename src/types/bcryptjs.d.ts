1deda71d3aabb1f34a63eba7b7e8e97b05d74850
Andy <andy.su7117@gmail.com>
2025-08-18 22:01:03 +1000

    Added bcryptjs declaration

--- src/types/bcryptjs.d.ts
+++ src/types/bcryptjs.d.ts
@@ -1,4 +1,3 @@
-// Minimal shim so TypeScript recognizes the bcryptjs module
 declare module "bcryptjs" {
   export function compare(data: string, encrypted: string): Promise<boolean>;
   export function hash(data: string, salt: number): Promise<string>;
